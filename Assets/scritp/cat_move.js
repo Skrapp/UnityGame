f33239db3aa3fb74c6d199f4a90fb02ecb2f08b0
nilssonsara97@gmail.com <nilssonsara97@gmail.com>
2015-12-17 22:55:11 +0100

    Bounces off enemy, enemy animation changing variables

--- Assets/scritp/cat_move.js
+++ Assets/scritp/cat_move.js
@@ -8,42 +8,43 @@ var x;
 var forceX= 1;
 var facingRight = true;
 var facingLeft = false;
-var cat_actions: cat_actions;  
+var cat_actions : cat_actions;  
 var thrust = 5000;
 var inputY : float;
+var enemyScript : enemy_behavior;
+var attacked = false;
 
 function Start () {
 
 }
 
 function Update () {
-    if (Input.GetKeyDown(KeyCode.W) && CanJump())
-    {
-        x = GetComponent.<Rigidbody2D>().velocity.x;
-        GetComponent.<Rigidbody2D>().velocity = new Vector2 (x, jumpHeight);
-        ++numJumps;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-        y = GetComponent.<Rigidbody2D>().velocity.y;
-        GetComponent.<Rigidbody2D>().velocity = new Vector2 (-speed, y);
+    if (!attacked) {
+        if (Input.GetKeyDown(KeyCode.W) && CanJump())
+        {
+            x = GetComponent.<Rigidbody2D>().velocity.x;
+            GetComponent.<Rigidbody2D>().velocity = new Vector2 (x, jumpHeight);
+            ++numJumps;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            y = GetComponent.<Rigidbody2D>().velocity.y;
+            GetComponent.<Rigidbody2D>().velocity = new Vector2 (-speed, y);
 
-        if (facingRight) {
-            Flip();
-        } 
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-        y = GetComponent.<Rigidbody2D>().velocity.y;
-        GetComponent.<Rigidbody2D>().velocity = new Vector2 (speed, y);
+            if (facingRight) {
+                Flip();
+            } 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            y = GetComponent.<Rigidbody2D>().velocity.y;
+            GetComponent.<Rigidbody2D>().velocity = new Vector2 (speed, y);
 
-        if (!facingRight) {
-            Flip();
-        }        
+            if (!facingRight) {
+                Flip();
+            }        
+        }
     }
-    /*var inputY = Input.GetAxis("Horizontal") * thrust * Time.deltaTime;
-    GetComponent.<Rigidbody2D>().AddForce(Vector2.up * 1000);
- Debug.Log (inputY);*/
 }
 
 function OnCollisionStay2D (coll : Collision2D) {
@@ -52,11 +53,13 @@ function OnCollisionStay2D (coll : Collision2D) {
     }
    
     if (coll.gameObject.CompareTag("Enemy")) {
-       /* inputY = Input.GetAxis("Horizontal") * thrust * Time.deltaTime;
-        Debug.Log (inputY);
-        GetComponent.<Rigidbody2D>().AddForce(Vector2.right * (-inputY) * thrust);
-        yield WaitForSeconds (10);*/
-        cat_actions.Death();
+
+        GetComponent.<Rigidbody2D>().velocity.y = 30f;
+        GetComponent.<Rigidbody2D>().velocity.x = enemyScript.bounceX;
+        attacked = true;
+        yield WaitForSeconds (1);
+        attacked = false;
+       // cat_actions.Death();
     }
 
 
@@ -78,5 +81,4 @@ function Flip () {
     rigidbody.transform.localScale = flipScale;
 
     facingRight =!facingRight;
-    //facingLeft = !facingLeft;
 }
--- Assets/scritp/enemy_behavior.js
+++ Assets/scritp/enemy_behavior.js
@@ -1,32 +1,31 @@
 ﻿#pragma strict
 var y;
 var speed = -10;
+var facingRight = true;
+var bounceX = 20f;
 
 function Start () {
 
 }
 
 function Update () {
-   /* y = GetComponent.<Rigidbody2D>().velocity.y;
-    GetComponent.<Rigidbody2D>().velocity = new Vector2 (speed, y);
-
-    if (transform.position.x < GameObject.Find("Left").transform.position.x){
-        //Debug.Log ("tja");
-        Flip();
-    }
-    if (transform.position.x > GameObject.Find("Right").transform.position.x ){
-        //Debug.Log ("tja1");
-        Flip();
-    }*/
 }
 
 function OnCollisionStay2D (coll : Collision2D) {
     if (coll.gameObject.CompareTag("EnemyDie")) {
         Debug.Log ("die potato");
     }
+    /*if (coll.gameObject.CompareTag("Player")) {
+        if (facingRight){
+        bounceX *= -1;
+        }
+        else {
+
+        }
+    }*/
 }
 
-function Flip () {
+/*function Flip () {
     var flipScale : Vector3;
     var rigidbody : Rigidbody2D;
 
@@ -40,4 +39,4 @@ function Flip () {
     rigidbody.transform.localScale = flipScale;
 
     transform.position.x += speed/10;
-}
+}*/
